5fca6b6c578a280d21321d984e8470d4506311dc
Sreelakshmi Kammath <skammath@omnissa.com>
2026-02-10 11:15:51 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+    public static void main(String a[]) {
+        int num1 = 3;
+        int num2 = 4;
+        int result = num1 + num2;
+        System.out.println(result);
+        System.out.println("Hello, World!");
+    }
+}
