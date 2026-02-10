4ddf8ba081c712b2b4a23ff059874bb61d154341
Sreelakshmi Kammath <skammath@omnissa.com>
2026-02-10 11:42:37 +0530

    Type conversion and Casting

--- Hello.java
+++ Hello.java
@@ -1,9 +1,19 @@
 class Hello {
     public static void main(String a[]) {
-        int num1 = 3;
-        int num2 = 4;
-        int result = num1 + num2;
-        System.out.println(result);
-        System.out.println("Hello, World!");
+        // int num1 = 3;
+        // int num2 = 4;
+        // int result = num1 + num2;
+        // System.out.println(result);
+        // System.out.println("Hello, World!");
+        byte n1 = 125;
+        int n2 = n1;
+        int n4 = 12;
+        int n5 = 257;
+        byte n3 = (byte) n4;
+        byte n6 = (byte) n5;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n6);
     }
 }
